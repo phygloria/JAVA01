2ae64dff1700b9a24c120c2930e60b123ba615f9
ploria <phygloria@gmail.com>
2024-04-11 18:02:02 +0900

    first commit

---
+++ chap01-literal-and-variable/src/main/java/com/ohgiraffers/section01/literal/Application01.java
@@ -0,0 +1,26 @@
+package com.ohgiraffers.section01.literal;
+
+public class Application01 {
+
+    public static void main(String[] args){
+        // 리터럴이란 값 자체를 의미한다.
+
+        // 정수형 리터럴
+        System.out.println(123);
+        // 실수형 리터럴
+        System.out.println(3.333);
+
+        // 문자형 리터럴
+        System.out.println('A');
+        // 문자열형 리터럴
+        System.out.println("Ab");
+        System.out.println("Hello World");
+
+        System.out.println("123.4");
+
+        // 논리형 리터럴
+        System.out.println(true);
+        System.out.println(false);
+
+    }
+}
---
+++ chap01-literal-and-variable/src/main/java/com/ohgiraffers/section01/literal/Application02.java
@@ -0,0 +1,35 @@
+package com.ohgiraffers.section01.literal;
+
+public class Application02 {
+    public static void main(String[] args){
+        // 2+2 , 2-2 , 2*2 , 2/2
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+
+        // 123 % 10
+        System.out.println(123 % 10);
+
+        // 1.23 + 1.23
+        // 1.23 - 1.23
+        // 1.23 * 1.23
+        // 1.23 / 1.23
+        // 1.23 % 1.0
+
+        System.out.println(1.23 + 1.23);
+        System.out.println(1.23 - 1.23);
+        System.out.println(1.23 * 1.23);
+        System.out.println(1.23 / 1.23);
+        System.out.println(1.23 % 1.0);
+
+        // "123" + "12"
+        System.out.println("123" + "12");
+        // "" 문자열이기 때문에 빼거나 곱하거나 나누거나 목연산x
+        System.out.println("123 12");
+        // "123" + 12 (문자열과 붙어서 문자열로 변환인식한다)
+        System.out.println("123"+12);
+
+
+    }
+}
